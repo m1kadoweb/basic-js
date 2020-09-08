37a5a30864a334e0649824329d1a4d6ac01ba8b0
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-09-08 18:01:27 +0500

    complete task what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/*date*/) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  const testDate = new Date(date)
+
+  if (date == undefined) {
+    return 'Unable to determine the time of year!'
+  }
+
+  if(Object.prototype.toString.call(date) === '[object Date]') {
+    if(date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+      return 'winter'
+    } else if(date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+      return 'spring'
+    } else if(date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+      return 'summer'
+    } else {
+      return 'autumn'
+    }
+  } else {
+      throw new ("Unable to determine the time of year!")
+    }        
+          
 };
