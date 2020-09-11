e2727fdaf47a1660e6375e37588989f50e6c6851
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-09-11 17:59:27 +0500

    Complete task hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) { 
+   
+  if(Array.isArray(arr)) {
+    if(arr.length != 0) {
+      let newArr = []
+      let tempArr = []
+
+      for(let i = 0; i < arr.length; i++) {
+        tempArr[i] = arr[i]
+      }
+
+
+
+
+
+      for(let i = 0; i < tempArr.length; i++) {
+        switch(tempArr[i]) {          
+          case '--discard-next':
+            if(i === tempArr.length-1) {
+              break
+            } else {
+              i = i + 1
+              break
+            }
+          case '--discard-prev': 
+            if(i === 0) {
+              break;
+            } else {
+              k--
+              newArr.pop()
+              break
+            }
+            
+          case '--double-next':
+            if(i === tempArr.length-1) {
+              break
+            } else {
+              newArr[k] = tempArr[i+1]
+              k++
+              break;
+            }
+          case '--double-prev':
+            if(i === 0) {
+              break
+            } else {
+              newArr[k] = tempArr[i-1]
+              k++
+              break
+            }
+          default:
+              newArr[k] = tempArr[i]
+              k++
+              break            
+        }
+
+       }
+       return newArr
+    } else {
+      return arr
+    }
+  } else {
+    throw new 'THROWN'
+  }
 };
