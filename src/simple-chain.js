c21074f8c45928118985a751f0f7a002ee7188aa
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2021-02-22 13:25:52 +0500

    complete chain maker task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chains: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      return this.chains.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.push("( " + value + " )~~");    
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position < 0 || position >= this.getLength) {
+      this.chains = [];
+      throw new Error("ERROR!");      
+    } else {
+      this.chains.splice(position-1, 1);
+    }    
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains[this.chains.length-1] = this.chains[this.chains.length-1].substring(0, this.chains[this.chains.length-1].length -2)
+    let result = this.chains.join("");
+    this.chains = [];
+    return result
   }
 };
 
